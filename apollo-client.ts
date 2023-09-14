562d578969cb0904135a9b1a4abf01e6b120108b
efracuadras <efra.cuadras@gmail.com>
2023-09-14 14:39:25 -0700

    fix: se cambio de ruta de servidor

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+// const URI = "https://rickandmortyapi.com/graphql"
+const URI = "http://ec2-52-26-247-226.us-west-2.compute.amazonaws.com:8002/graphql"
 const client = new ApolloClient({
-    uri: "https://rickandmortyapi.com/graphql",
+    uri: URI,
     cache: new InMemoryCache(),
 });
 
